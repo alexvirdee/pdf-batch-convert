fc5bd8abd17144314f5ce54c0c6d46c491e97e4a
Alex Virdee <alex.virdee08@gmail.com>
2023-07-15 20:49:38 -0400

    Check files to process

--- src/app/components/DragAndDrop.tsx
+++ src/app/components/DragAndDrop.tsx
@@ -2,12 +2,28 @@
 import Dropzone from "react-dropzone"
 
 export default function DragAndDrop() {
+    const onDrop = (acceptedFiles) => {
+        // Filter out non-PDF files
+        const pdfFiles = acceptedFiles.filter(file => file.type === 'application/pdf');
+
+        if (pdfFiles.length === 0) {
+            console.log('no pdf files were selected');
+            return
+        }
+
+        // Process the selected PDF files
+        console.log('Selected PDF files', pdfFiles);
+        // Call function to handle the selected files
+        // pass to back end python program for processing 
+
+    }
+
+
     return (
         <>
-            <div>Hello from drag and drop!</div>
-            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            <Dropzone onDrop={onDrop}>
                 {({ getRootProps, getInputProps }) => (
-                    <section>
+                    <section className="dropZone">
                         <div {...getRootProps()}>
                             <input {...getInputProps()} />
                             <p>Drag 'n' drop some files here, or click to select files</p>
